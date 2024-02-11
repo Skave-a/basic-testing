e948fb2427da5482817788d9e66acb9ccbf4fdbb
Skave-a <arrowtata@gmail.com>
2024-02-11 21:33:46 +0300

    fix: snapshot

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,29 +1,51 @@
-import axios from 'axios';
-// import { throttledGetDataFromApi } from './index';
-
-jest.mock('axios');
+import axios, { Axios } from 'axios';
+import { THROTTLE_TIME, throttledGetDataFromApi } from './index';
 
 const baseURL = 'https://jsonplaceholder.typicode.com';
+const relativePath = '/posts';
+const responseData = [{ id: 1, name: 'name' }];
 
 describe('throttledGetDataFromApi', () => {
-  test('should create instance with provided base url', async () => {
-    axios.create({ baseURL: baseURL });
-
-    expect(axios.create).toHaveBeenCalledWith({ baseURL: baseURL });
+  beforeAll(() => {
+    jest.useFakeTimers();
   });
 
-  test('should perform request to correct provided url', async () => {
-    // const path = '/posts';
-    // jest.spyOn(axios, 'get');
-    // await throttledGetDataFromApi(path);
-    // expect(axios.get).toHaveBeenCalledWith(path);
+  afterAll(() => {
+    jest.useRealTimers();
   });
 
-  test('should return response data', async () => {
-    // const data = { id: 1, title: 'post' };
-    // jest.spyOn(axios, 'get').mockResolvedValue({ data: data });
-    // const relativePath = '/posts';
-    // const data = await throttledGetDataFromApi(relativePath);
-    // expect(data).toEqual(data);
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    jest.advanceTimersByTime(THROTTLE_TIME);
+
+    jest.spyOn(axios, 'create');
+
+    jest
+      .spyOn(Axios.prototype, 'get')
+      .mockResolvedValueOnce({ data: responseData });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should create an instance with the provided base URL', async () => {
+    await throttledGetDataFromApi(relativePath);
+
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL,
+    });
+  });
+
+  test('should perform a request to the correct provided URL', async () => {
+    await throttledGetDataFromApi(relativePath);
+
+    expect(Axios.prototype.get).toHaveBeenCalledWith(relativePath);
+  });
+
+  test('should return the response data', async () => {
+    const result = await throttledGetDataFromApi(relativePath);
+
+    expect(result).toEqual(responseData);
   });
 });
--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,21 +1,31 @@
 import { generateLinkedList } from './index';
 
-const values = ['1', '2', '3', '4', '5'];
+const values = [1, 2, 3, 4, 5];
 
 describe('generateLinkedList', () => {
   test('should generate linked list from values 1', () => {
-    const value = ['1'];
-
     const expectedLinkedList = {
-      value: '1',
+      value: 1,
       next: {
-        value: null,
-        next: null,
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: 4,
+            next: {
+              value: 5,
+              next: {
+                value: null,
+                next: null,
+              },
+            },
+          },
+        },
       },
     };
 
-    const linkedList = generateLinkedList(value);
-    expect(linkedList).toEqual(expectedLinkedList);
+    const linkedList = generateLinkedList(values);
+    expect(linkedList).toStrictEqual(expectedLinkedList);
   });
 
   test('should generate linked list from values 2', () => {
